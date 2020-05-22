825214aaccc80c64bc472142c432db80fde195d7
piyush <piyush@piyush-ubuntu>
2020-05-22 21:48:56 +0530

    commit

--- ClientLogin_MicroService/src/main/java/com/pineapple/soft/ClientLoginMicroServiceApplication.java
+++ ClientLogin_MicroService/src/main/java/com/pineapple/soft/ClientLoginMicroServiceApplication.java
@@ -2,7 +2,7 @@ package com.pineapple.soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hhghg
 @SpringBootApplication
 public class ClientLoginMicroServiceApplication {
 
